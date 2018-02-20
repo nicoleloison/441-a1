7c1305c210290b054741bfd05e4ab8effa4406e6
nicole <garance.loison@gmail.com>
2018-02-20 01:45:28 -0500

    not receiving, sending file

--- client_udp.c
+++ client_udp.c
@@ -6,11 +6,14 @@
 #include <resolv.h>
 #include<netinet/in.h>
 #include<sys/types.h>
+#include <string.h>
+#include <errno.h>
 
 
 int main(){
-    int clientSocket, portNum, nBytes;
-    char buffer[1024];
+    
+    int clientSocket, from_server,to_server;
+    char buffer[2048];
     struct sockaddr_in serverAddr;
     socklen_t addr_size;
     
@@ -19,39 +22,44 @@ int main(){
     
     /*Configure settings in address struct*/
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8000);
+    serverAddr.sin_port =htons(8000);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");/*IP adress at university: 136.159.195.213*/
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
     
     /*Initialize size variable to be used later on*/
     addr_size = sizeof serverAddr;
     
+    
     while(1){
         
         printf("Type the name of the file to transfer\n");
         scanf("%s",buffer);
         
-        // send  msg to server
-        
+        /*send  req of file to server*/
         sendto(clientSocket, buffer, strlen(buffer), 0,
                (struct sockaddr *)&serverAddr, sizeof(struct sockaddr));
         
-        char file_buffer[2000];
-        
-        if (recvfrom(clientSocket,file_buffer,2000,0,  (struct sockaddr *)&serverAddr, sizeof(struct sockaddr))<0)
+        char file_buffer[2048];
+        from_server=recvfrom(clientSocket, buffer, 2048, 0,(struct sockaddr *)&serverAddr,sizeof(struct sockaddr));
+        while(from_server>0)
         {
-            printf("error in recieving the file\n");
-            exit(1);
+            write(1, file_buffer, from_server);
+        }
+        
+        if (from_server<0)
+        {   printf("ERROR: %s\n", strerror(errno));
+            break;
         }
         
         char new_file[]="copied";
-        strcat(new_file,buffer);
+        strcat(new_file,file_buffer);
        
         FILE *fp;
         fp=fopen(new_file,"w+");
         if(fwrite(file_buffer,1,sizeof(file_buffer),fp)<0)
         {
             printf("error writting file\n");
+            printf("ERROR: %s\n", strerror(errno));
             exit(1);
         }
         
--- server_udp.c
+++ server_udp.c
@@ -7,16 +7,16 @@
 #include<fcntl.h>
 #include<sys/types.h>
 #include<string.h>
+#include <errno.h>
 
 int main(){
     
-    
-    int udpSocket, nBytes;
-    char buffer[1024];
-    char file_buffer[2000];
+    int udpSocket, from_client, to_client;
+    char buffer[2048];
+    char file_buffer[1024];
     struct sockaddr_in serverAddr, clientAddr;
     struct sockaddr_storage serverStorage;
-    socklen_t addr_size;
+    socklen_t addr_size, cl_addr_size;
 
     /*---- Create the socket. The three arguments are: ----*/
     /* 1) Internet domain 2) Stream socket 3) Default protocol (UDP in this case) */
@@ -34,15 +34,14 @@ int main(){
     
      /*---- Bind the address struct to the socket ----*/
     bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-    
     
     /*Initialize size variable to be used later on*/
     addr_size = sizeof serverStorage;
+    cl_addr_size = sizeof clientAddr;
     
     while(1){
         
-        nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+        from_client = recvfrom(udpSocket,buffer,2048,0,(struct sockaddr *)&serverStorage, &addr_size);
         printf("connected to client: file to send %s \n",buffer);
         
         /* SENDING FILE*/
@@ -51,20 +50,32 @@ int main(){
         if(fp==NULL)
         {
             printf("file does not exist\n");
+            printf("ERROR: %s\n", strerror(errno));
         }
         
         fseek(fp,0,SEEK_END);
         size_t file_size=ftell(fp);
         fseek(fp,0,SEEK_SET);
+   
         
         if(fread(file_buffer,file_size,1,fp)<=0)
         {
-            printf("unable to copy file into buffer\n");
-            return 0;
+            printf("file could not be copied.\n");
+            printf("ERROR: %s\n", strerror(errno));
+            break;
         }
         
-        if(sendto(udpSocket,file_buffer,strlen(file_buffer),0,  (struct sockaddr *)&clientAddr, &addr_size)<0){
-            printf("error in sending the file\n");
+        char copy[1024];
+        /* make sure strlen(name) >= 3 */
+        strncpy(copy, file_buffer, 1024);
+    
+        
+        to_client = sendto(udpSocket,copy,1024,0,(struct sockaddr *)&clientAddr, cl_addr_size);
+       
+        if(to_client <0){
+            printf("file could not be sent.\n");
+            printf("ERROR: %s\n", strerror(errno));
+            printf("file length: %s\n",file_size);
             break;
         }
         
